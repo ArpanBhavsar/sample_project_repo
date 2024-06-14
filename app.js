f41342f6c27a9a4daf95e97f5494b5510c624e14
Arpan Bhavsar <arpanbhavsar.160410116019@gmail.com>
2024-06-13 17:27:48 -0700

    Created the express server and created the home route to test with Hello World.

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+        res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+        console.log(
+          `Express server started listening on port ${PORT}. \nTo access this Node.js app open URL: http://localhost:${PORT}`
+        );
+      });
